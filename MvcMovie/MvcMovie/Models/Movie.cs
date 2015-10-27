da01d556d84e5610a2b267c9302138ccf27d2a5f
irreversible <irreversible>
2015-10-27 18:05:52 +0600

    Code Added

---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+          [Required]
+        public string Genre { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Range(1,100)]
+        public Decimal Price { get; set; }
+        [StringLength(5)]
+        public string Rating { get; set; }
+
+       
+    }
+
+
+    public class MovieDBContext : DbContext
+    {
+
+        public DbSet<Movie> Movies { get; set; }
+
+
+    }
+}
